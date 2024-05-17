7c693fd4efa1bafaee40570649f4870860d1dab2
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-16 21:56:55 -0500

    Added premake5.lua  file

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Llama"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Llama"
+	location "Llama"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+	objdir ("bin-int/" ..outputdir.. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	include
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"LLAMA_PLATFORM_WINDOWS",
+			"LLAMA_BUILD_DLL",
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "LLAMA_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "LLAMA_RELEASE"
+		optimize "On"
+
+	files "configurations:Dist"
+		defines "LLAMA_DIST"
+		optimize "On"
+
+	LLAMA_PLATFORM_WINDOWS;LLAMA_BUILD_DLL;_WINDLL;
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+	objdir ("bin-int/" ..outputdir.. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	include
+	{
+		"%{prj.name}/vendor/spdlog/include",
+		"Llama/src"
+	}
+
+	links
+	{
+		"Llama"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"LLAMA_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "LLAMA_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "LLAMA_RELEASE"
+		optimize "On"
+
+	files "configurations:Dist"
+		defines "LLAMA_DIST"
+		optimize "On"
